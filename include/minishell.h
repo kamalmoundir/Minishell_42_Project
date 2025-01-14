bb345426e364901926c660f3b9a41ee2b698e922
Rachid Jaada <rjaada42@gmail.com>
2025-01-14 17:29:38 +0100

    updating the header in echo

--- include/minishell.h
+++ include/minishell.h
@@ -3,24 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmoundir <kmoundir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rjaada <rjaada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 18:08:22 by rjaada            #+#    #+#             */
-/*   Updated: 2025/01/14 16:47:28 by kmoundir         ###   ########.fr       */
+/*   Updated: 2025/01/14 17:26:42 by rjaada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "../libraries/libft/libft.h"
 # include <readline/history.h>
 # include <readline/readline.h>
+# include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
-# include <signal.h>
-
-# include  "../libraries/libft/libft.h"
 
 // utils
 int		ft_strcmp(const char *s1, const char *s2);
@@ -34,8 +33,7 @@ char	*expand_variable(const char *token);
 // error handling
 int		error_input(char quote);
 
-
-//builtins
-int	echo(int ac, char **av, int fd);
+// builtins
+int		echo(int ac, char **av, int fd);
 
 #endif
--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmoundir <kmoundir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rjaada <rjaada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 14:28:03 by kmoundir          #+#    #+#             */
-/*   Updated: 2025/01/14 16:26:56 by kmoundir         ###   ########.fr       */
+/*   Updated: 2025/01/14 17:28:23 by rjaada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../../include/minishell.h"
 
 int	echo(int ac, char **av, int fd)
 {
@@ -27,7 +27,7 @@ int	echo(int ac, char **av, int fd)
 	while (i < ac - 1)
 	{
 		ft_putstr_fd(av[i], fd);
-		if(av[i + 1])
+		if (av[i + 1])
 			ft_putstr_fd(" ", fd);
 		i++;
 	}
@@ -39,13 +39,14 @@ int	echo(int ac, char **av, int fd)
 	}
 	return (0);
 }
+
 /*int	main(int ac, char **av)
 {
-	
 	if (ac >= 1)
 	{
 		echo(ac, (av + 1), 1);
 	}
-	else ft_putchar_fd('\n', 1);
+	else
+		ft_putchar_fd('\n', 1);
 	return (0);
 }*/
