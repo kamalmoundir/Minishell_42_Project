a7db0e0726ed0769a31992e6bea1c6492619cfe0
Rachid Jaada <rjaada42@gmail.com>
2025-01-15 23:40:20 +0100

    add comments

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -3,40 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmoundir <kmoundir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rjaada <rjaada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 18:48:04 by kmoundir          #+#    #+#             */
-/*   Updated: 2025/01/15 11:09:51 by kmoundir         ###   ########.fr       */
+/*   Updated: 2025/01/15 23:37:14 by rjaada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "minishell.h"
 #include <linux/limits.h>
 
-int pwd(int number_arg)
+int	pwd(int number_arg)
 {
-    char *cwd;
-    int error_num;
-    
-    if(number_arg == 0)
-    {
-        cwd = getcwd(NULL,PATH_MAX);
-        if(!cwd)
-        {
-            error_num = errno;
-            ft_putstr_fd("pwd:",2);
-            ft_putstr_fd(strerror(error_num), 2);
-            ft_putstr_fd("\n",2);                    
-        }
-        else
-        {
-            printf("%s\n", cwd);
-            free(cwd);
-        }
-    }
-    else
-        ft_putstr_fd("Too many arguments\n", 2);
+	char	*cwd;
+	int		error_num;
 
-    return 0;
+	if (number_arg == 0)
+	{
+		cwd = getcwd(NULL, PATH_MAX);
+		if (!cwd)
+		{
+			error_num = errno;
+			ft_putstr_fd("pwd:", 2);
+			ft_putstr_fd(strerror(error_num), 2);
+			ft_putstr_fd("\n", 2);
+		}
+		else
+		{
+			printf("%s\n", cwd);
+			free(cwd);
+		}
+	}
+	else
+		ft_putstr_fd("Too many arguments\n", 2);
+	return (0);
 }
--- src/executions.c
+++ src/executions.c
@@ -6,10 +6,10 @@
 /*   By: rjaada <rjaada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 17:34:55 by rjaada            #+#    #+#             */
-/*   Updated: 2025/01/15 17:44:28 by rjaada           ###   ########.fr       */
+/*   Updated: 2025/01/15 23:40:10 by rjaada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
+/*l9it prob kifech integre echo o pwd fl code dyali, o taniyan dert wa7ed une petite optimisation fl code bach tkoun more readable */
