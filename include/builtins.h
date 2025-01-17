181de88e651a8d69a905ea8d7272b4e7af5d9b24
kamalmoundir <moundirkamal@gmail.com>
2025-01-17 12:52:43 +0100

    add gnl

---
+++ include/builtins.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmoundir <kmoundir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/17 11:51:31 by kmoundir          #+#    #+#             */
+/*   Updated: 2025/01/17 11:52:56 by kmoundir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTINS_H
+# define BUILTINS_H
+ 
+// builtins
+int			echo(int ac, char **av, int fd);
+int			pwd(int number_arg);
+
+#endif
